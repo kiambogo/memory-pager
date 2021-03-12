fa2199be35cd35145a6fa47025f5de92feabddc1
Christopher Poenaru <kiambogo@gmail.com>
2021-03-12 08:06:16 -0800

    add methods to implement

--- pager.go
+++ pager.go
@@ -4,7 +4,7 @@ const DEFAULT_PAGE_SIZE = 1024
 
 type Page struct {
 	offset int
-	buffer *[]byte
+	buffer []byte
 }
 
 type Pager struct {
@@ -21,8 +21,29 @@ func NewPager(pageSize int) *Pager {
 	}
 }
 
+// Get will return the page at the specified index
 func (p *Pager) Get(i int) *Page {
 	return nil
+}
+
+// GetOrAlloc will return the page at the specified index, allocating it if not already allocated
+func (p *Pager) GetOrAlloc(i int) *Page {
+	return nil
+}
+
+// PageSize will return the page size of the pager
+func (p *Pager) PageSize() int {
+	return p.pageSize
+}
+
+// Len will return the size of the pager (number of pages)
+func (p *Pager) Len() int {
+	return len(p.pages)
+}
+
+// IsEmpty will check if the memory page is empty (has zero pages)
+func (p *Pager) IsEmpty() bool {
+	return len(p.pages) == 0
 }
 
 func (p *Pager) Set(index int, data []byte) {
@@ -30,11 +51,9 @@ func (p *Pager) Set(index int, data []byte) {
 	page.buffer = p.truncate(data)
 }
 
-func (p *Pager) ToBuffer() {}
-
-func (p *Pager) grow(index int) {
-	newPageListSize := len(p.pages) * 2
-	for newPageListSize <= index
+func (p *Pager) resize(index int) {
+	// newPageListSize := len(p.pages) * 2
+	//	for newPageListSize <= index
 }
 
 func (p Pager) truncate(buf []byte) []byte {
